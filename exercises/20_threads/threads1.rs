df7db149f59dfc528164ebbcbfef384e480d5d3d
AndyRightNow <naruto199507@gmail.com>
2024-12-10 17:51:49 +0900

    update

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -24,6 +24,9 @@ fn main() {
     for handle in handles {
         // TODO: Collect the results of all threads into the `results` vector.
         // Use the `JoinHandle` struct which is returned by `thread::spawn`.
+        if let Result::Ok(r) = handle.join() {
+            results.push(r);
+        }
     }
 
     if results.len() != 10 {
